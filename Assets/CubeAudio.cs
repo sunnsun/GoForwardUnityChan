4551f243a9dff7f0c90cccf42c5570c7139cf48a
DESKTOP-074UFLO\すんすん <sunstyle17@gmail.com>
2017-07-17 19:15:40 +0900

    Lesson7 課題 課題の確認をお願いいたします。 音を鳴らすために、if文で条件付けし、作成しました。

---
+++ Assets/CubeAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//衝突判定を行い、衝突したものがCubeもしくはGroundの場合、音を鳴らす。　というスクリプトを作成する
+
+//if文もしくはbool型を使用する
+
+//if文で作成（bool型はいろいろ試すも、うまいこといかず。。）
+   
+public class CubeAudio : MonoBehaviour {
+    
+    private AudioSource sound;
+
+    public GameObject cubePrefab;
+
+    public GameObject Ground;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        sound = GetComponent<AudioSource>();
+        if(other.gameObject.tag == "GroundTag") 
+        {
+            sound.Play();
+        }
+        if(other.gameObject.tag == "CubePrefab")
+        {
+            sound.Play();
+        }
+    }
+}
---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private float speed = -0.2f;
+
+    private float deadline = -10f;
+
+
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.speed, 0, 0);
+        if (this.transform.position.x < this.deadline)
+        {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
+
